261832b7acd6839f0f8926c6e864c86bbde83a35
Tram Anh Nguyen <tramanhxuannguyen0304@gmail.com>
2023-12-12 16:33:33 +0700

    Initial commit

---
+++ src/Controls/GamePanel.java
@@ -0,0 +1,82 @@
+package Controls;
+
+import java.awt.*;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    //Screen settings
+    final int originalTileSize = 16; // 16x16 tiles
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale; // 48x48 tiles
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    //FPS
+    int FPS = 60;
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+    //Player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension (screenWidth, screenHeight));
+        //this.setBackground();
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+
+    }
+
+    public void startGameThread(){
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000/FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while(gameThread != null){
+            //Update info such as char position
+            update();
+            //Draw the screen with the updated info
+            repaint();
+
+        }
+    }
+    public void update(){
+        if(keyH.upPressed){
+            playerY -= playerSpeed;
+        }
+        else if (keyH.downPressed){
+            playerY += playerSpeed;
+        }
+        else if (keyH.leftPressed){
+            playerY -= playerSpeed;
+        }
+        else if (keyH.rightPressed){
+            playerY += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        // g2.setColor(Color.white);
+        // g2.fillRect(playerX,playerY,tileSize,tileSize);
+        // g2.dispose();
+
+
+
+    }
+}
---
+++ src/Entity/Player.java
@@ -0,0 +1,18 @@
+package Entity;
+
+import Controls.GamePanel;
+import Controls.KeyHandler;
+
+public class Player extends Entity {
+
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public Player(GamePanel gp, KeyHandler keyH){
+        this.gp = gp;
+        this.keyH = keyH;
+
+    }
+
+
+}
