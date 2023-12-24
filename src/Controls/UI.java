91ee4eec62f54077fe1fee0f856c503ff8783d2d
ltbngoc7 <ltbngoc2004@gmail.com>
2023-12-24 11:46:30 +0700

    pause screen w font vt323

--- src/Controls/UI.java
+++ src/Controls/UI.java
@@ -2,7 +2,6 @@ package Controls;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,55 +10,40 @@ import java.awt.Font;
 public class UI {
     GamePanel gp;
     Graphics2D g2;
-    Font arial_40;
-    public BufferedImage image;
-    public UI(GamePanel gp){
-        this.gp = gp;
+    Font customFont;
 
-        arial_40 = new Font("Serif", Font.PLAIN, 40);
+    public UI(GamePanel gp) {
+        this.gp = gp;
+        loadFont();
     }
-    public void draw(Graphics2D g2 ){
+
+    private void loadFont() {
+        try (InputStream fontStream = getClass().getResourceAsStream("/Background/VT323-Regular.ttf")) {
+            customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics2D g2){
 
         this.g2 = g2;
-
-        g2.setFont(arial_40 );
-        g2.setColor(Color.white);
 
         if(gp.gameState == gp.playState) {
             // Do playSate stuff later
         }
         if(gp.gameState == gp.pauseState) {
             drawPauseScreen();
-            //g2.drawImage(image, gp.screenWidth, gp.screenHeight, null);
-        //}
-        //if (gp.gameState == gp.pauseState) {
-            //drawPauseScreen();
-            //if (image != null) {
-                //g2.drawImage(image, gp.screenWidth, gp.screenHeight, null);
-            //}
         }
     }
 
     public void drawPauseScreen() {
 
-        //try (InputStream filePause = getClass().getResourceAsStream("res/Background/pause_screen.png")){
-            //if (filePause != null) {
-            //assert filePause != null;
-            //image = ImageIO.read(filePause);
-                //g2.drawImage(image, gp.screenWidth, gp.screenHeight, null);
-            //} else {
-                //System.err.println("Image not found!");
-            //}
-        //}catch (IOException e){
-            //e.printStackTrace();
-
-        //}
-
         g2.setColor(new Color(128, 128, 128, 200)); // Grey color with alpha (transparency)
         g2.fillRect(0, 0, gp.getWidth(), gp.getHeight()); // Fill the entire screen
 
         g2.setColor(Color.white);
-        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 80F));
+        g2.setFont(customFont.deriveFont(Font.PLAIN, 90f));
         String text = "PAUSED";
         int x = getXforCenteredText(text);
         int y = gp.screenHeight/2;
