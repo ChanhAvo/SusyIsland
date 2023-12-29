6b9041c2d820202ae7c4d763fd8564c562fcb44b
ltbngoc7 <ltbngoc2004@gmail.com>
2023-12-29 15:02:28 +0700

    resize heart

--- src/Object/OBJ_Heart.java
+++ src/Object/OBJ_Heart.java
@@ -15,8 +15,8 @@ public class OBJ_Heart extends SuperObject {
         this.gp = gp;
 
         name = "Heart";
-        try (InputStream fileHeartFull = new FileInputStream("res/Objects/full heart.png");
-             InputStream fileHeartHalf = new FileInputStream("res/Objects/half.png");
+        try (InputStream fileHeartFull = new FileInputStream("res/Objects/heart_full.png");
+             InputStream fileHeartHalf = new FileInputStream("res/Objects/heart_half_full.png");
              InputStream fileHeartBlank = new FileInputStream("res/Objects/die.png")) {
             image = ImageIO.read(fileHeartFull);
             image2 = ImageIO.read(fileHeartHalf);
